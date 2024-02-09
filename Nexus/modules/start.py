b86c9e3d81511fee9914659d5f25867b143ad586
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-09 20:43:50 +0530

    Update start.py

--- Nexus/modules/start.py
+++ Nexus/modules/start.py
@@ -5,27 +5,37 @@ from pyrogram.types import InlineKeyboardButton as ib
 import asyncio
 from Nexus import Nexus as app
 
-start_txt = """
-IAM NOOB 
+
+START_TEXT = """
+ʜᴇʟʟᴏ {} 
+\n⋆─────────────────────⋆
+Wᴇʟᴄᴏᴍᴇ \n⋆─────────────────────⋆
+Iᴍᴍᴇʀsᴇ ʏᴏᴜʀsᴇʟғ ɪɴ ᴀ ᴡᴏʀʟᴅ ᴏғ ᴍᴜsɪᴄ ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ\n⋆─────────────────────⋆
+Dɪsᴄᴏᴠᴇʀ, ᴘʟᴀʏ, ᴀɴᴅ ᴇɴJᴏʏ ʏᴏᴜʀ ғᴀᴠᴏʀɪᴛᴇ ᴛᴜɴᴇs ʀɪɢʜᴛ ʜᴇʀᴇ\n⋆─────────────────────⋆
+Sɪᴍᴘʟʏ sᴇɴᴅ ᴍᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏғ ᴛʜᴇ sᴏɴɢ ᴏʀ ᴀʀᴛɪsᴛ, ᴀɴᴅ ʟᴇᴛ ᴛʜᴇ ᴍᴇʟᴏᴅʏ ʙᴇɢɪɴ. \n⋆─────────────────────⋆
+Usᴇ Help ғᴏʀ ᴍᴏʀᴇ ᴄᴏᴍᴍᴀɴᴅs. 🎶
 """
+# ------------------------------------------------------------------------------- #
+
+
 
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
     buttons = [
         [
-            InlineKeyboardButton("🎩ADD ME BABY🪄", callback_data="admin_data")
+            InlineKeyboardButton("⦿ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ⦿", callback_data="admin_data")
         ],
         [
-            InlineKeyboardButton("🆘𝖲𝖴𝖯𝖱𝖮𝖳🆘", url="https://t.me/{SUPPORT}"),
-            InlineKeyboardButton("🧑‍💻𝖣𝖤𝖵🧑‍💻", user_id=OWNER_ID)
+            InlineKeyboardButton("⦿ɢʀᴏᴜᴘ⦿", url="https://t.me/{SUPPORT}"),
+            InlineKeyboardButton("⦿ᴏᴡɴᴇʀ⦿", user_id=OWNER_ID)
         ]
     ]
 
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await message.reply_video(
-        video="https://telegra.ph/file/630e51ee72baec5dc9012.mp4",
-        caption=start_txt,
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=START_TEXT,
         reply_markup=reply_markup
     )
